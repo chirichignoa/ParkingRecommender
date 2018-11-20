b1af94b78ac050f31bb4983f95352b21d53d7357
chiri <chirichignoa@gmail.com>
2018-11-20 15:06:07 -0300

    ParkingMetersService - Port Propertie

--- ParkingMetersService/src/main/java/Application.java
+++ ParkingMetersService/src/main/java/Application.java
@@ -1,18 +1,38 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import parkingmeters_service.ParkingMetersService;
 import util.Path;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class Application {
 
+    private String URL = "";
+    private static Logger log = LogManager.getLogger(Application.class);
+
     public Application(){
         this.init();
     }
 
     private void init() {
-        port(4567);
-        get(Path.PARKING_METERS, ParkingMetersService::getAllParkingMeters);
+        Properties prop = new Properties();
+        InputStream is;
+        try {
+            is =this.getClass().getClassLoader().getResourceAsStream("properties.properties");
+            prop.load(is);
+            int port = Integer.parseInt(prop.getProperty("app.port"));
+            port(port);
+            log.debug("Running at port: " + port);
+            get(Path.PARKING_METERS, ParkingMetersService::getAllParkingMeters);
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }
+
     }
 
     public static void main(String[] args) {
